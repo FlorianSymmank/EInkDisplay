c0ded0e7f06a8ed7f830efbdb0a821dbdf5b48e1
FlorianSymmank <floriansymmank@gmx.de>
2024-04-11 16:53:39 +0200

    client accepts paras to send

--- client.py
+++ client.py
@@ -1,5 +1,18 @@
 import socket
+import time
+import sys
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('zero', 8089))
-clientsocket.send('hello'.encode('UTF-8'))
+texts = []
+
+if len(sys.argv) > 1:
+    for arg in sys.argv[1:]:
+        texts.append(arg)
+else:
+    texts.append("Default Text")
+
+for text in texts:
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect(('zero', 8089))
+    clientsocket.send(text.encode('UTF-8'))
+    clientsocket.close()
+    time.sleep(0.5)
